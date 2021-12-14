0cd98c2c7dd9b7cc1239c7a8df23e72b2b50d093
rafajlpz <frilo_94@hotmail.com>
2021-12-14 12:49:26 +0100

    ejercicios 18

---
+++ Ejercicio1/codigo.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("JELOUUU MUNDO!!");
+console.log("Soy el primer script");
+
+// COMENTARIOS //
+    // -> Primero se quita el "<script></script>" del archivos index.html, se escribe en la cabezera "<script src="/Ejercicio1/codigo.js"></script>" para linkear el archivo javascript "codigo.js"
+    // -> Despues se se escribe el codigo en javascript, por orden de preferencia se escriben los console.log "hola mundo" y "soy el primer script"
+    
+
+alert("Activa la consola");
