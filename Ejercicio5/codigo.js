afabc121115898ac7cc169b90da793b94f719059
Rafael <frilo_@hotmail.com>
2021-12-19 19:37:38 +0100

    Actualizacion

---
+++ Ejercicio5/codigo.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var numero1 = 5;
+var numero2 = 8;
+
+if(numero1<numero2){
+    console.log(`${numero1} no es mayor que ${numero2}`)
+}
+
+if(numero2>0){
+    console.log(`${numero2} es positivo`)
+}
+
