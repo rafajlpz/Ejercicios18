484b3d22c77ac7ece3956a80b6c48e4d12181642
Rafael <frilo_@hotmail.com>
2021-12-19 20:10:58 +0100

    Actaluzacion hasta el Ej 5

--- Ejercicio5/codigo.js
+++ Ejercicio5/codigo.js
@@ -11,3 +11,10 @@ if(numero2>0){
     console.log(`${numero2} es positivo`)
 }
 
+if(numero1<0 || numero1>0){
+    console.log(`${numero1} es negativo o distinto de 0`)
+}
+
+if(numero1++>numero2){
+    console.log(`Incrementar en 1 el valor de ${numero1} no lo hace superior a ${numero2}`)
+}
