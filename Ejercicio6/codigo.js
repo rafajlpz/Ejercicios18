f71efc75d9e6ed0daf804d650b35dbbf88dda3ea
RAFA <frilo_94@hotmail.com>
2021-12-24 19:47:56 +0100

    Actualizacion Ejercicio 6 DNI. -Creacion y finalizacion ejercicio 7 factorial.

--- Ejercicio6/codigo.js
+++ Ejercicio6/codigo.js
@@ -1,8 +1,6 @@
 "use strict";
 
-var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
-
-var DNI = prompt("Introduce DNI sin letra");
+// const DNI = prompt("Introduce DNI sin letra");
 //var letra = prompt("Introduce letra de DNI");
 
 // if(isNaN(DNI)===false){ //es número
@@ -16,13 +14,48 @@ var DNI = prompt("Introduce DNI sin letra");
 
 // }
 
-let regDni = /^[0-9]{1,8}$/;
-if(regDni.test(DNI)){
-    DNI = Number(DNI);
-    alert(DNI)
-}else{
-    alert("Introduce un numero válido")
+
+
+// let regDni = /^[0-9]{8}$/;
+// if(regDni.test(DNI)){
+//     DNI = Number(DNI);
+//     alert(DNI)
+// }else{
+//     alert("Introduce un numero válido")
+// }
+
+
+const calcular = (param) => {
+    if (isNaN(param)) {
+        alert("No es un numero")
+        calcular(param);
+    } else {
+        if (param < 0 || param > 99999999) {
+            return "";
+        } else {
+            const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+            const resto = param % 23;
+            const letra = letras[resto];
+            return letra;
+        }
+    }
+
 }
+
+let quieresSerguir = true;
+    while(quieresSerguir){
+        const n = prompt('Introduce numero para obtener la letra.');
+        const letra = calcular(n);
+        alert(letra);
+        quieresSerguir=confirm("Quieres seguir haciendo calculos?");
+    }
+
+
+
+//var regLetra = prompt("Introduce letra");
+
+
+//typeof(redLetra);
 
 // if (DNI < 0 || DNI > 99999999) {
 //     alert("El numero proporcionado no es valido");
---
+++ Ejercicio7/codigo.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var miNumero = 4;
+var resultado = miNumero;
+
+for (let i = 1; i < miNumero; i++){
+    resultado=resultado*i
+};
+console.log(resultado);
