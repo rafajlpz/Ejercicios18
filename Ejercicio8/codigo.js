eb166a1ebe546c50f2c8aa442de141ded34d7de9
RAFA <frilo_94@hotmail.com>
2021-12-25 18:24:36 +0100

    Ejercicio 8
    Funcion numero par o impar

---
+++ Ejercicio8/codigo.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const parImpar = () => {
+    if (numero % 2 == 0) {
+        alert("El numero es par");
+    } else {
+        alert("El numero es impar");
+    }
+}
+
+var numero = prompt('Introduce numero')
+var resultado = parImpar(numero);
+
+
+
