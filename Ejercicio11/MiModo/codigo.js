0a74d4f8e85c1a6eae78a71f14bfcab927cdc726
RAFA <frilo_94@hotmail.com>
2022-01-06 07:10:50 +0100

    -m -Creacion array personas e instancia de objetos. -Creacion de blue for para conseguir que muestre en pantalla los detalles de cada persona -Se coge el output en el HTML.

--- Ejercicio11/MiModo/codigo.js
+++ Ejercicio11/MiModo/codigo.js
@@ -7,7 +7,53 @@ class Persona{
     this.edad = edad;
     this.genero = genero;
     }
-    obtDetalleS() {
+    obtDetalles() {
+        return `
+            <ul class="campos">
+                <li class="campo">
+                    Nombre: ${this.nombre}
+                </li>
+                <li class="campo">
+                    Edad: ${this.edad}
+                </li>
+                <li class="campo">
+                    Genero: ${this.genero}
+                </li>
+            </ul>
+            `;
+    }
+}
+class Estudiante extends Persona{
+    constructor(curso, grupo){
+        super(nombre, edad, genero);
+
+        this.curso = curso;
+        this.grupo = grupo;
+    }
+    registrar(){
         
     }
+}
+class Profesor extends Persona{
+    constructor(asignatura, nivel){
+        super(nombre, edad, genero)
+
+        this.asignatura = asignatura;
+        this.nivel = nivel;
+    }
+    asignar(){
+        
+    }
+}
+
+const output = document.querySelector(".output");
+
+let personas = [
+    new Persona ('Rafael', 27, 'M'),
+    new Persona ('Valeria', 22, 'F')
+];
+
+for(let i = 0; i<personas.length; i++){
+    output.innerHTML += personas[i].obtDetalles(); //Que muestre cada persona que hay en personas y muestre sus detalles con la funciona obDetalles. //
+    //Con el + en innerHTML obtenemos en este caso las dos personas.
 }
